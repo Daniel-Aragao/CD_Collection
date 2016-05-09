f07403aefcda6b4f7484470a61bd5745b11acaa1
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-05-08 23:49:17 -0300

    primeira cara da aplicação
    correção das configurações do JSF

--- src/cdc/entitys/CD.java
+++ src/cdc/entitys/CD.java
@@ -9,4 +9,45 @@ public class CD {
 	private Timestamp data_compra;
 	private String descricao;
 	private Double preco_compra;
+	
+	public CD(){
+		
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getCod_label() {
+		return cod_label;
+	}
+	public void setCod_label(int cod_label) {
+		this.cod_label = cod_label;
+	}
+	public Timestamp getData_gravacao() {
+		return data_gravacao;
+	}
+	public void setData_gravacao(Timestamp data_gravacao) {
+		this.data_gravacao = data_gravacao;
+	}
+	public Timestamp getData_compra() {
+		return data_compra;
+	}
+	public void setData_compra(Timestamp data_compra) {
+		this.data_compra = data_compra;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getPreco_compra() {
+		return preco_compra;
+	}
+	public void setPreco_compra(Double preco_compra) {
+		this.preco_compra = preco_compra;
+	}
 }
--- src/cdc/entitys/beams/CDForm.java
+++ src/cdc/entitys/beams/CDForm.java
@@ -3,15 +3,29 @@ package cdc.entitys.beams;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name="cdForm")
+import cdc.entitys.CD;
+
+@ManagedBean(name="CDForm")
 @RequestScoped
 
 public class CDForm {
-		
+	private CD cdSearch;
 	
 	
 	public CDForm (){
-		
+		cdSearch = new CD();
+	}
+
+//	public ArrayList search()
+	
+
+	public CD getCdSearch() {
+		return cdSearch;
+	}
+
+
+	public void setCdSearch(CD cdSearch) {
+		this.cdSearch = cdSearch;
 	}
 	
 	
