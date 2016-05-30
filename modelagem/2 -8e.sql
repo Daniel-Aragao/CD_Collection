271869e25e0e37feeb6eeafa2db16888875aff81
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-05-30 12:56:39 -0300

    8e

--- modelagem/2 -8e.sql
+++ modelagem/2 -8e.sql
@@ -1,12 +1,20 @@
---SELECT *  FROM CD cd
---INNER JOIN
---(
+SELECT cd.* FROM CD cd
 
---SELECT f.codigo, f.codigo_CD, f.descricao, f.numero, f.tipo_composicao, f.tipo_composicao, f.tipo_gravacao FROM Faixa f
---JOIN Tipo_de_Composicao tc ON tc.codigo = f.tipo_composicao
---JOIN Faixa_por_compositor fc on fc.codigo_faixa = f.codigo
---JOIN Compositor c on c.codigo = fc.codigo_compositor
---JOIN Periodo_Musical p on p.codigo = c.codigo_periodo_musical
---WHERE p.descricao = 'Barroco' AND tc.descricao = 'Concerto'
+EXCEPT
 
---) result on result.codigo_CD = cd.codigo
+SELECT cd.*  FROM CD cd
+INNER JOIN
+(
+
+SELECT f.* FROM Faixa f
+JOIN Tipo_de_Composicao tc ON tc.codigo = f.tipo_composicao
+JOIN Faixa_por_compositor fc on fc.codigo_faixa = f.codigo
+JOIN Compositor c on c.codigo = fc.codigo_compositor
+JOIN Periodo_Musical p on p.codigo = c.codigo_periodo_musical
+WHERE p.descricao != 'Barroco' OR tc.descricao != 'Concerto'
+
+) result on result.codigo_CD = cd.codigo
+
+
+-- criar cd com duas faixas onde todas são barroco(3) e concerto(4) 
+-- e um cd com 3 onde uma não é barroco ou o concerto
