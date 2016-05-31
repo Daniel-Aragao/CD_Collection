566cd4dcdda93067292f68d7de332b57c28517a7
Nedehf <nedehf@edu.unifor.br>
2016-05-31 14:18:54 -0300

    Atualização 4 a

--- modelagem/2 - 4 a.sql
+++ modelagem/2 - 4 a.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER Faixa_TR_periodo_barroco
 	
 	IF (SELECT tipo_gravacão FROM Inserted) <> 'DDD'
 	AND (SELECT descricao FROM Periodo_Musical INNER JOIN Compositor ON (codigo = codigo_periodo_musical) 
-	INNER JOIN Faixa_por_compositor ON (codigo = codigo_compositor) INNER JOIN Inserted ON (codigo_faixa = codigo) = 'Barroco'
+	INNER JOIN Faixa_por_compositor ON (codigo = codigo_compositor) INNER JOIN Inserted ON (codigo_faixa = codigo)) = 'Barroco'
 	BEGIN 
 		RAISERROR ('Faixa do Periodo Barroco, necessita gravacao tipo DDD')
 		ROLLBACK TRANSACTION
