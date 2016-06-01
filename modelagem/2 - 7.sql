c40064c15108532e70c5e00ae6ca43d4a006a1c8
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-06-01 11:06:41 -0300

    last

--- modelagem/2 - 7.sql
+++ modelagem/2 - 7.sql
@@ -1,6 +1,11 @@
-CREATE VIEW nome_grav_qtd_cds
-as
-SELECT g.nome Nome_gravadora, COUNT (*) Qtd_CDs 
-FROM Label g, CD 
+CREATE VIEW nome_grav_qtd_cdf(Nome_gravadora, Qtd_CDs) WITH SCHEMABINDING
+AS
+
+SELECT g.nome Nome_gravadora, COUNT_BIG (*) Qtd_CDs 
+FROM dbo.Label g, dbo.CD 
 WHERE CD.cod_label = g.codigo
 GROUP BY g.nome
+
+
+
+CREATE UNIQUE CLUSTERED INDEX I_nome_grav_qtd_cd ON nome_grav_qtd_cdf(Nome_gravadora)
