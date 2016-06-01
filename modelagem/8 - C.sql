046a06daa49b419f3f2274004fb9ab6d8be8f419
Nedehf <nedehf@edu.unifor.br>
2016-06-01 00:20:18 -0300

    Novo 8 c

--- modelagem/8 - C.sql
+++ modelagem/8 - C.sql
@@ -1,4 +1,8 @@
 SELECT (*) 
 FROM CD 
-WHERE (SELECT nome 
-FROM Compositor c INNER JOIN Faixa f ON (c.codigo = f.codigo_compositor) INNER JOIN CD d ON(f.codigo_CD = d.codigo)) = 'Bach'
+WHERE 'Bach' = (SELECT nome 
+FROM Compositor c 
+INNER JOIN Faixa_por_compositor fc ON (c.codigo = fc.codigo_compositor)
+INNER JOIN Faixa f ON (fc.codigo_faixa = f.codigo) 
+INNER JOIN  CD d ON (f.codigo_CD = d.codigo)
+)
