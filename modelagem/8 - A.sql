ecd07d0afc6614b15afda27d57ccf2c76fdc931f
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-06-01 01:18:07 -0300

    8

--- modelagem/8 - A.sql
+++ modelagem/8 - A.sql
@@ -1,1 +1,12 @@
-SELECT nome FROM Label WHERE MAX( SELECT AVG(c.preco_compra) FROM CD c, Label l WHERE c.cod_label = l.codigo)
+SELECT l.nome FROM Label l
+JOIN 
+(SELECT l.codigo, AVG(cd.preco_compra) as media  FROM Label l
+JOIN CD cd ON cd.cod_label = l.codigo
+GROUP BY l.codigo) m ON m.codigo = l.codigo
+
+WHERE m.media = 
+
+(SELECT max(m.media) FROM 
+(SELECT l.codigo, AVG(cd.preco_compra) as media  FROM Label l
+JOIN CD cd ON cd.cod_label = l.codigo
+GROUP BY l.codigo) m)
